f0cb7085fa11c2910ec839a7e02f85d2b26f84c5
Brandon Morrell <brandon@morrellworld.net>
2021-03-14 18:17:11 -0500

    Added Character Anims

--- Project Entropy/Assets/PlayerMovement.cs
+++ Project Entropy/Assets/PlayerMovement.cs
@@ -6,17 +6,19 @@ public class PlayerMovement : MonoBehaviour
 {
     public float moveSpeed = 4f;
     public float blinkSpeed = 2.5f;
-
+    public Animator anim;
     public bool isDashing = false;
-
     public Rigidbody2D rb;
-
+  
     Vector2 movement;
     
     void Update()
     {
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
+        int dirX = (int)Mathf.Ceil(movement.x) + 1;
+        int dirY = (int)Mathf.Ceil(movement.y) + 1;
+        anim.SetInteger("RunningInt", dirX * 3 + dirY);
         if (Input.GetKeyDown("left shift"))
         {
             isDashing = true;
@@ -32,4 +34,5 @@ public class PlayerMovement : MonoBehaviour
             isDashing = false;
         }
     }
+
 }
