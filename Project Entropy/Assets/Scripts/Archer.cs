9078511afd7f93fbf7431b4e4381fe0a4cdc40a6
Brandon Morrell <brandon@morrellworld.net>
2021-04-19 13:11:25 -0500

    Archers and Arrows vUpdated

--- Project Entropy/Assets/Scripts/Archer.cs
+++ Project Entropy/Assets/Scripts/Archer.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -8,16 +9,24 @@ public class Archer : MonoBehaviour
     public GameObject target;
     private float timer;
     private int waitingTime;
+    SpriteRenderer spriteRenderer;
     // Start is called before the first frame update
     void Start()
     {
         timer = .5f;
         waitingTime = 3;
+        spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (Math.Abs(transform.rotation.eulerAngles.z) > 90 && Math.Abs(transform.rotation.eulerAngles.z) < 270)
+            spriteRenderer.flipY = true;
+        
+        else
+            spriteRenderer.flipY = false;
+
         Vector3 direction = target.transform.position - transform.position;
         float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
 
@@ -25,7 +34,7 @@ public class Archer : MonoBehaviour
 
         float distance = Vector2.Distance(transform.position, target.transform.position);
         timer += Time.deltaTime;
-        if (timer > waitingTime && distance < 10)
+        if (timer > waitingTime && distance < 15)
         {
             GameObject arrowSpawn = Instantiate(arrow, transform.position, transform.rotation);
             timer = 1;
